adba33526a41ab771cc7cf14260b51914b337253
José Augusto <jose.augusto@tbit.com.br>
2022-10-14 14:29:26 -0300

    setup and requirements

--- image-processing-package/setup.py
+++ image-processing-package/setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image-processing",
     version="0.0.1",
-    author="my_name",
+    author="José Augusto",
     author_email="my_email",
-    description="My short description",
+    description="Test version - Image processing. This package is a demo for simulation of upload on the Test Pypi website.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/jaugustorc/Criacao-de-pacotes-em-Python.git"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
